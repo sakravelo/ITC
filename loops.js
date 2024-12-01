f1c5903005926b16e82ec30c59d90f44e41ec829
sakravelo <sakravelo@addu.edu.ph>
2024-12-01 21:43:48 +0800

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,28 +1,27 @@
-document.getElementById("compute").addEventListener("click", function () {
-        // Get taxable income and implicitly convert to number
-        var it;
-        let ti = document.getElementById("tax").value*1;
+document.getElementById("calculate").addEventListener("input", function() {
+    var n = document.getElementById("calculate").value * 1;
 
-        // Calculate income tax based on the tax brackets
-        if (ti < 250000) {
-            it = 0; // No tax
-        } else if (ti <= 400000) {
-            it = 0.20 * (ti - 250000);
-        } else if (ti <= 800000) {
-            it = 30000 + 0.25 * (ti - 400000);
-        } else if (ti <= 2000000) {
-            it = 130000 + 0.30 * (ti - 800000);
-        } else if (ti <= 8000000) {
-            it = 490000 + 0.32 * (ti - 2000000);
-        } else {
-            it = 2410000 + 0.35 * (ti - 8000000);
-        }
+    let factorial = 1;
+    let i = 1;
+    while (i <= n) {
+        factorial *= i;
+        i++;
+    }
 
-        // Display the result
-        document.getElementById("incometax").value = it.toFixed(2);
-    });
+    let sum = 0;
+    let j = 1;
+    do {
+        sum += j;
+        j++;
+    } while (j <= n);
 
-    document.getElementById("clear").addEventListener("click", function () {
-        document.getElementById("tax").value ='';
-        document.getElementById("incometax").value ='';
-    });
+    let average = 0;
+    for (let k = 1; k <= n; k++) {
+        average += k;
+    }
+    average = average / n;
+
+    document.getElementById("fact").innerHTML = "The factorial of the first " + n + " natural numbers is " + factorial;
+    document.getElementById("sum").innerHTML = "The sum of the first " + n + " natural numbers is " + sum;
+    document.getElementById("ave").innerHTML = "The average of the first " + n + " natural numbers is " + average;
+});
